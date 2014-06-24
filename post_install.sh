5c236aebb08029ad3f341f9d75363ff51b080fc1
YarnSeemannsgarn <janschlenker1990@gmail.com>
2014-06-24 21:58:09 +0200

    Add .gitignore and post_install script

---
+++ post_install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+sudo apt-get update
+
+sudo apt-get install emacs24 -y
+
+#TODO: find and add config file
+sudo apt-get install nautilus-dropbox -y
+
+#TODO: add config file /keepassx/config.ini
+sudo apt-get install keepassx -y
+ 
+#TODO: add config file ~/.config/chromium
+sudo apt-get install chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg -y
+sudo apt-get install chromium-codecs-ffmpeg-extra -y
+
+sudo apt-get install git -y
+git config --global user.name YarnSeemannsgarn
+git config --global user.email janschlenker1990@gmail.com
+git config --global color.ui "auto"
+git config --global pack.threads "0"  
+cd ~
+git init
+git remote add origin https://github.com/YarnSeemannsgarn/ubuntu-config
+# Instead of pull use fetch and reset to overwrite local files
+git fetch --all
+git reset --hard origin/master
+
+sudo apt-get update
