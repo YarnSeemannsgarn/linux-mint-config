ac3d174c7d04f5af4a70bcbe2d9c48078859367c
YarnSeemannsgarn <janschlenker1990@gmail.com>
2014-07-02 16:59:19 +0200

    No use so far for virtualbox-guest-additions-iso
    - Copy and paste is possible without addition

--- post_install.sh
+++ post_install.sh
@@ -62,13 +62,3 @@ sudo apt-get -y install build-essential dkms
 sudo apt-get install -y virtualbox virtualbox-qt virtualbox-dkms virtualbox-guest-dkms
 sudo adduser $USER vboxusers
 newgrp - vboxusers
-sudo apt-get -y install virtualbox-guest-additions-iso 
-echo "You have to mount the .iso file under /usr/share/virtualbox/ via VirtualBox, to enable guest additions (e.g. Copy & Paste). 
-
-Afterwards you can install the guest addition on the host.
-Windows: The .exe file is part of the CD/DVD drive
-Linux: Execute 'sudo sh ./VBoxLinuxAdditions.run' under a mounted CD/DVD drive
-
-"
-read -p "Press any key to continue... " -n1 -s
-
