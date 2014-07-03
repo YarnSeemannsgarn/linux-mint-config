b4625c2d91f1eda8dad2e351fdd29d98816d9dd2
YarnSeemannsgarn <janschlenker1990@gmail.com>
2014-07-03 21:48:19 +0200

    Add virtual box guest addition

--- post_install.sh
+++ post_install.sh
@@ -62,6 +62,20 @@ sudo apt-get -y install build-essential dkms
 sudo apt-get install -y virtualbox virtualbox-qt virtualbox-dkms virtualbox-guest-dkms
 sudo adduser $USER vboxusers
 newgrp - vboxusers
+sudo apt-get -y install virtualbox-guest-additions-iso 
+echo "You have to mount the .iso file under /usr/share/virtualbox/ via VirtualBox, to enable guest additions (e.g. Copy & Paste). 
+
+Afterwards you can install the guest addition on the host.
+Windows: The .exe file is part of the CD/DVD drive
+Linux: Execute 'sudo sh ./VBoxLinuxAdditions.run' under a mounted CD/DVD drive
+
+"
+read -p "Press any key to continue... " -n1 -s
+echo "Windows programm list for VirtualBox:
+  - Antivir
+  - Elster
+  - Dropbox (only sync necessary files e.g. Elster formulas)"
+read -p "Press any key to continue... " -n1 -s
 
 # Burn programme - defaultly included in Ubuntu, but noteworthy for other distributions
 sudo apt-get -y install brasero
