de02fa63db148d306f81b67f3a932e16008d1098
YarnSeemannsgarn <janschlenker1990@gmail.com>
2017-11-23 09:44:49 +0100

    Fixes installation of mercurial

--- post_install/smart_dato_inst.sh
+++ post_install/smart_dato_inst.sh
@@ -23,7 +23,7 @@ while true; do
 
             . $DIR/docker_inst.sh
 	    
-            sudo apt-get -y install hg
+            sudo apt-get -y install mercurial
 
             sudo apt-get -y install sudo apt-get install pgadmin3
 
