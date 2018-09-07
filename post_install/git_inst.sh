fa6d45dd8b476c2706eaa211c5146cb5cc9a0f59
YarnSeemannsgarn <janschlenker1990@gmail.com>
2018-09-07 19:55:13 +0200

    Adds public key to GitHub programmatically

--- post_install/git_inst.sh
+++ post_install/git_inst.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+# Generate private/public key pair
 ssh-keygen -t rsa -C $EMAIL
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_rsa
-echo "Add public rsa key to GitHub manually, to enable git pushes without password request. You can find add instructions under https://help.github.com/articles/generating-ssh-keys#step-3-add-your-ssh-key-to-github"
-read -p "Press any key to continue... " -n1 -s
+
+# Add public key to GitHub
+curl -u "${EMAIL}" --data "{\"title\":\"$(whoami)@$(hostname)\",\"key\":\"$(cat ${HOME}/.ssh/id_rsa.pub)\"}" https://api.github.com/user/keys
 ssh -T git@github.com
 
+# Install git
 sudo apt-get -y install git
+
+# Download this repository into the correct location
 cd
 git init
-git remote add origin git@github.com:YarnSeemannsgarn/ubuntu-config.git
+git remote add origin git@github.com:YarnSeemannsgarn/linux-mint-config.git
 # Instead of pull use fetch and reset to overwrite local files
 git fetch --all
 git reset --hard origin/master
