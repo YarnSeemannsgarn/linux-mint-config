6768eba359945652b0fc0903b390f2cdec143d4c
YarnSeemannsgarn <janschlenker1990@gmail.com>
2018-09-08 21:38:42 +0200

    Replaces unzip by unzzip

--- post_install/exodus_inst.sh
+++ post_install/exodus_inst.sh
@@ -3,6 +3,6 @@
 cd /opt/
 file_name=exodus-linux-x64-${EXODUS_VERSION}
 sudo wget https://exodusbin.azureedge.net/releases/${file_name}.zip
-sudo unzip ${file_name}.zip
+sudo unzzip ${file_name}.zip
 sudo chown -R $USER:$USER Exodus-linux-x64*
 sudo rm ${file_name}.zip
