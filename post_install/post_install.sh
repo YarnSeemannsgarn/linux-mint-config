c44cb33a3cd530d42281f0f1b7ca6fd3c263a2fd
YarnSeemannsgarn <janschlenker1990@gmail.com>
2018-09-08 12:44:09 +0200

    Comments out brasero

--- post_install/post_install.sh
+++ post_install/post_install.sh
@@ -46,8 +46,8 @@ sudo apt-get -y install gimp
 
 sudo apt-get -y install texlive texlive-lang-german texlive-latex-extra lmodern latex-xcolor pgf
 
-# Burn programme - defaultly included in Ubuntu, but noteworthy for other distributions
-sudo apt-get -y install brasero
+# Burn programme - defaultly included in Ubuntu, but noteworthy for other distributions --> depends on machine
+# sudo apt-get -y install brasero
 
 # Simple image editor like paint
 sudo apt-get -y install pinta
