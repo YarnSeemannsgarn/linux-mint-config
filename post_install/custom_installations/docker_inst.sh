82712e7ae0acb3845ddae6c57635e24623a9ce04
YarnSeemannsgarn <janschlenker1990@gmail.com>
2018-09-09 20:39:35 +0200

    Adapt docker-compose installation command

--- post_install/custom_installations/docker_inst.sh
+++ post_install/custom_installations/docker_inst.sh
@@ -27,8 +27,7 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 
 # Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Bash completion
