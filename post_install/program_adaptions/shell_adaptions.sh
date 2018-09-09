5e134478306af192ac680a0b7aa2eac82eb798a8
YarnSeemannsgarn <janschlenker1990@gmail.com>
2018-09-09 17:42:02 +0200

    Add oh-my-zsh

--- post_install/program_adaptions/shell_adaptions.sh
+++ post_install/program_adaptions/shell_adaptions.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Add adaptions to .basrhc and .zshrc
-cat $PROGRAM_ADAPT_DIR/.shellrc >> $HOME/.bashrc
-cat $PROGRAM_ADAPT_DIR/.shellrc >> $HOME/.zshrc
-
 # Change default shell to zsh
 chsh -s /usr/bin/zsh
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Add adaptions to .basrhc and .zshrc (after oh-my-zsh!)
+cat $PROGRAM_ADAPT_DIR/.shellrc_custom >> $HOME/.bashrc
+cat $PROGRAM_ADAPT_DIR/.shellrc_custom >> $HOME/.zshrc
+cat $PROGRAM_ADAPT_DIR/.bashrc_custom >> $HOME/.bashrc
+cat $PROGRAM_ADAPT_DIR/.zshrc_custom >> $HOME/.zshrc
