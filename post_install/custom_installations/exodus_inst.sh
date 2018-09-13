80544a971e669199470bfd84569716196d6bd147
YarnSeemannsgarn <janschlenker1990@gmail.com>
2018-09-13 13:18:15 +0200

    Remove previous exodus dir when installing new exodus version

--- post_install/custom_installations/exodus_inst.sh
+++ post_install/custom_installations/exodus_inst.sh
@@ -2,7 +2,9 @@
 
 cd /opt/
 file_name=exodus-linux-x64-${EXODUS_VERSION}
+dir_name=Exodus-linux-x64
+sudo rm -rf $dir_name
 sudo wget https://exodusbin.azureedge.net/releases/${file_name}.zip
 sudo unzip ${file_name}.zip
-sudo chown -R $USER:$USER Exodus-linux-x64*
+sudo chown -R $USER:$USER $dir_name
 sudo rm ${file_name}.zip
