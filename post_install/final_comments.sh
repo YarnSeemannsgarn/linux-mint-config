a1f0570b9717da2a6f553717248a1411ebea2981
YarnSeemannsgarn <janschlenker1990@gmail.com>
2020-01-26 19:07:34 +0100

    Remove comment about DOCEAR... who needs this crab

--- post_install/final_comments.sh
+++ post_install/final_comments.sh
@@ -8,13 +8,6 @@ read -p ""
 echo "UNI INNSBRUCK VPN CLIENT:"
 echo "Offers client via https://vpn.uibk.ac.at/+CSCOE+/portal.html, but cannot be downloaded directly"
 echo "Alternative: openconnect, Problem: no working networkmanager"
-read -p "" 
-
-echo "DOCEAR:"
-echo "What a mess to install"
-echo "Lots of JAVA Errors (seems to work only with openjdk8 http://www.docear.org/faqs/what-should-i-know-when-using-docear-on-a-linux-system/)"
-echo "Seems to be also a problem when using Arch AUR package"
 read -p ""
-
 
 
